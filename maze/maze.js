9691de17d78c29c7105ec17cf3e2753904443bf4
YmmSgy <briansujl@gmail.com>
2023-10-17 10:29:05 +0800

    major refactor
    - enhanced code readability and efficiency

--- maze/maze.js
+++ maze/maze.js
@@ -1,184 +1,180 @@
 "use strict";
 
-const t0 = Date.now();
+// performs in-place shuffle of array arr
+function shuffle(arr) {
+  // Fisher-Yates shuffle
 
-function Cd(x, y) {
+  // Initially, the pool is the entire array. On each iteration, a random
+  // element in the pool is swapped with the last element of the pool, and the
+  // last element is removed from the pool. This continues until the pool is
+  // left with only the first element.
+
+  // iterate from back to front
+  for (let i = arr.length - 1; i > 0; --i) {
+    // generate random index within the pool (0 to i inclusive)
+    const r = Math.floor(Math.random() * (i + 1));
+
+    // swap items at r and i
+    const tmp = arr[r];
+    arr[r] = arr[i];
+    arr[i] = tmp;
+  }
+}
+
+class Vec2 {
+  x;
+  y;
+
+  static zero = new Vec2(0, 0);
+  static north = new Vec2(0, -1);
+  static south = new Vec2(0, 1);
+  static east = new Vec2(1, 0);
+  static west = new Vec2(-1, 0);
+
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.equals = function (othercd) {
-        return this.x === othercd.x && this.y === othercd.y;
+  }
+  static equals(a, b) {
+    return a.x === b.x && a.y === b.y;
+  }
+  static add(a, b) {
+    return new Vec2(a.x + b.x, a.y + b.y);
+  }
+}
+
+class Maze {
+  width;
+  height;
+  tiles;
+  visitedTiles;
+
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+
+    // initialise tiles with top and left walls
+    this.tiles = [];
+    for (let y = 0; y < this.width; ++y) {
+      this.tiles.push([]);
+      for (let x = 0; x < this.height; ++x) {
+        this.tiles[y][x] = { topWall: true, leftWall: true };
+      }
     }
-    this.add = function (othercd) {
-        return new Cd(this.x + othercd.x, this.y + othercd.y);
+
+    // set starting tile for maze generation
+    this.visitedTiles = [new Vec2(0, 0)];
+  }
+
+  // creates a new maze
+  generate() {
+    // copy the starting tile from visited tiles
+    const activeTiles = this.visitedTiles.slice();
+
+    // repeat while there are still more paths to explore
+    while (activeTiles.length > 0) {
+      // set current tile as the latest active tile
+      const curTile = activeTiles.at(-1);
+
+      // shuffle directions
+      const dirs = [Vec2.north, Vec2.south, Vec2.west, Vec2.east];
+      shuffle(dirs);
+
+      // find a valid direction from current tile
+      const foundDir = dirs.find((dir) => this.isValid(Vec2.add(curTile, dir)));
+
+      // if valid direction was found
+      if (foundDir != null) {
+        // link the tiles in that direction
+        this.removeWall(curTile, foundDir);
+
+        // record the new tile for backtracking and path exclusion
+        const nextTile = Vec2.add(curTile, foundDir);
+        activeTiles.push(nextTile);
+        this.visitedTiles.push(nextTile);
+      } else {
+        // backtrack if valid direction was not found
+        activeTiles.pop();
+      }
     }
-    this.scale = function (factor)  {
-        return new Cd(this.x * factor, this.y * factor);
+  }
+
+  removeWall(curTile, dir) {
+    // remove the respective wall from current tile
+    if (Vec2.equals(dir, Vec2.north)) {
+      this.tiles[curTile.y][curTile.x].topWall = false;
+    } else if (Vec2.equals(dir, Vec2.south)) {
+      this.tiles[curTile.y + 1][curTile.x].topWall = false;
+    } else if (Vec2.equals(dir, Vec2.west)) {
+      this.tiles[curTile.y][curTile.x].leftWall = false;
+    } else if (Vec2.equals(dir, Vec2.east)) {
+      this.tiles[curTile.y][curTile.x + 1].leftWall = false;
     }
-}
-const cd_n      = new Cd(0, -1);
-const cd_e      = new Cd(1, 0);
-const cd_s      = new Cd(0, 1);
-const cd_w      = new Cd(-1, 0);
-const cd_ne     = cd_n.add(cd_e);
-const cd_se     = cd_s.add(cd_e);
-const cd_sw     = cd_s.add(cd_w);
-const cd_nw     = cd_n.add(cd_w);
+  }
 
-const mazewidth = 40;
-const mazeheight = 40;
+  // check if the given tile can be linked to
+  isValid(tile) {
+    // to be valid, tile must not equal any of the visited tiles
+    const isVisited = this.visitedTiles.some((visitedTile) =>
+      Vec2.equals(tile, visitedTile)
+    );
 
-function shufflearr(arr) {
-    for (let rem = arr.length; rem > 0; rem--) {
-        // get random index from remaining pool
-        const r = Math.floor(Math.random() * rem);
-        // swap random from pool with last in pool
-        const temp = arr[rem - 1];
-        arr[rem - 1] = arr[r];
-        arr[r] = temp;
-    }
-}
+    // to be valid, tile must be within range
+    const withinRangeX = 0 <= tile.x && tile.x < this.width;
+    const withinRangeY = 0 <= tile.y && tile.y < this.height;
 
-function Nextdir() {
-    const arr = [ cd_e, cd_s, cd_w, cd_n ];
-    shufflearr(arr);
-    this.get = function () { return arr.pop(); };
-    this.isempty = function () { return arr.length === 0; };
-}
+    return !isVisited && withinRangeX && withinRangeY;
+  }
 
-function list_latest() {
-    return this[this.length - 1]
-}
-function list_includes(x) {
-    for (const el of this) {
-        if (el.equals(x)) return true;
-    }
-    return false;
-}
+  // draw the maze on the given canvas
+  draw(ctx, strokeStyle, lineWidth) {
+    const tileW = ctx.canvas.width / this.width;
+    const tileH = ctx.canvas.height / this.height;
+    ctx.strokeStyle = strokeStyle;
+    ctx.fillStyle = strokeStyle;
+    ctx.lineWidth = lineWidth;
 
-const activetiles = [];
-activetiles.push(new Cd(
-    Math.floor(Math.random() * mazewidth),
-    Math.floor(Math.random() * mazeheight)
-));
-activetiles.latest = list_latest;
-activetiles.includes = list_includes;
-
-const visitedtiles = [activetiles[0]];
-visitedtiles.latest = list_latest;
-visitedtiles.includes = list_includes;
-
-function Edge(a, b) {
-    if      (a.x < b.x) { this.a = a; this.b = b; }
-    else if (a.x > b.x) { this.a = b; this.b = a; }
-    else if (a.y < b.y) { this.a = a; this.b = b; }
-    else if (a.y > b.y) { this.a = b; this.b = a; }
-    else                { this.a = a; this.b = b; }
-    this.equals = function (otheredge) {
-        return this.a.equals(otheredge.a) && this.b.equals(otheredge.b);
-    };
-}
-
-const walls = [];
-walls.removewall = function (tile, dir) {
-    function tiletoedge(tile, dir) {
-        if (dir.equals(cd_n)) {
-            return new Edge(tile, tile.add(cd_e));
-        }
-        else if (dir.equals(cd_e)) {
-            return new Edge(tile.add(cd_e), tile.add(cd_se));
-        }
-        else if (dir.equals(cd_s)) {
-            return new Edge(tile.add(cd_se), tile.add(cd_s));
-        }
-        else if (dir.equals(cd_w)) {
-            return new Edge(tile.add(cd_s), tile);
-        }
-        else {
-            console.log('line 96');
-        }
-    }
-    const removedwall = tiletoedge(tile, dir);
-    const iremovedwall = this.findIndex((wall) => removedwall.equals(wall));
-    this.splice(iremovedwall, 1);
-}
-for (let y = 0; y < mazeheight + 1; y++) {
-    for (let x = 0; x < mazewidth; x++) {
-        walls.push(new Edge(
-            new Cd(x, y), new Cd(x + 1, y)
-        ));
-    }
-}
-for (let x = 0; x < mazewidth + 1; x++) {
-    for (let y = 0; y < mazeheight; y++) {
-        walls.push(new Edge(
-            new Cd(x, y), new Cd(x, y + 1)
-        ));
-    }
-}
-
-const ctx = document.getElementById('canvas').getContext('2d');
-const linewidth = 2;
-const corigin = new Cd(linewidth / 2, linewidth / 2);
-const cwidth = ctx.canvas.width - linewidth;
-const cheight = ctx.canvas.height - linewidth;
-const twidth = Math.floor(cwidth / mazewidth);
-const theight = Math.floor(cheight / mazeheight);
-
-function clearmaze() {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, cwidth, cheight);
-}
-
-function drawmaze() {
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = linewidth;
-    ctx.lineCap = 'square';
-
-    ctx.beginPath();
-    for (const wall of walls) {
-        ctx.moveTo(
-            corigin.x + wall.a.x * twidth,
-            corigin.y + wall.a.y * theight
+    // for every tile
+    for (let y = 0; y < this.height; ++y) {
+      for (let x = 0; x < this.width; ++x) {
+        // fill in the top left corner
+        ctx.fillRect(
+          x * tileW - lineWidth / 2,
+          y * tileH - lineWidth / 2,
+          lineWidth,
+          lineWidth
         );
-        ctx.lineTo(
-            corigin.x + wall.b.x * twidth,
-            corigin.y + wall.b.y * theight
-        );
-    }
-    ctx.stroke();
-}
 
-function algostep() {
-    const nextdircarousel = new Nextdir();
-    // repeat until valid direction found
-    while (true) {
-        if (nextdircarousel.isempty()) {
-            // backtrack
-            activetiles.pop();
-            break;
+        // draw the top wall or the left wall, or both
+        const tile = this.tiles[y][x];
+        if (tile.topWall) {
+          ctx.beginPath();
+          ctx.moveTo(x * tileW, y * tileH);
+          ctx.lineTo((x + 1) * tileW, y * tileH);
+          ctx.stroke();
         }
-        const nextdir = nextdircarousel.get();
-        const nextpos = activetiles.latest().add(nextdir);
-        const isnextposvalid =
-            0 <= nextpos.x && nextpos.x < mazewidth &&
-            0 <= nextpos.y && nextpos.y < mazeheight;
-        if (!visitedtiles.includes(nextpos) && isnextposvalid) {
-            // move to untouched tile
-            walls.removewall(activetiles.latest(), nextdir);
-            activetiles.push(nextpos);
-            visitedtiles.push(nextpos);
-            break;
+        if (tile.leftWall) {
+          ctx.beginPath();
+          ctx.moveTo(x * tileW, y * tileH);
+          ctx.lineTo(x * tileW, (y + 1) * tileH);
+          ctx.stroke();
         }
+      }
     }
+
+    // draw a border around the maze
+    ctx.lineWidth = lineWidth * 2;
+    ctx.strokeRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
 }
 
-// maze algorithm
-while (activetiles.length !== 0) {
-    algostep();
-}
+const ctx = document.getElementsByTagName("canvas")[0].getContext("2d");
+const maze = new Maze(16, 16);
 
-clearmaze();
-drawmaze();
+const t0 = Date.now();
+
+maze.generate();
+maze.draw(ctx, "black", 2);
 
 const t1 = Date.now();
 console.log(`maze generated in ${t1 - t0}ms`);
